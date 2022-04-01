6dcb32abd2d4fae55fbbf20c669ce8a4514269fe
SlackToTheMax <caizhaoan123@gmail.com>
2022-04-01 21:00:37 +0800

    Humidity, temperature and CO2 up

--- bleSub.py
+++ bleSub.py
@@ -12,7 +12,7 @@ ACCESS_TOKEN = 'vsydo22hiEvrYXeg9Kmw'
 
 INTERVAL=2
 
-sensor_data = {'temperature': 0, 'humidity': 0}
+sensor_data = {'temperature': 0, 'humidity': 0, 'co2':0}
 
 next_reading = time.time() 
 
@@ -33,13 +33,22 @@ p = btle.Peripheral("4C:75:25:CB:80:A2")
 svc = p.getServices()
 s1 = p.getServiceByUUID(list(svc)[2].uuid)
 c1 = s1.getCharacteristics()[0]   
-c2 = s1.getCharacteristics()[1]   
+c2 = s1.getCharacteristics()[1]
+
+
+p2 = btle.Peripheral("4C:75:25:CB:89:9A")
+
+svc2 = p2.getServices()
+s2 = p2.getServiceByUUID(list(svc2)[2].uuid)
+co2 = s2.getCharacteristics()[0]   
+
  
 while True:
-    if p.waitForNotifications(20000):
-        print("Humidity: ", c1.read().decode(), " Temperature Celsius: ", c2.read().decode())
+    if p.waitForNotifications(20000) or p2.waitForNotifications(20000):
+        print("Humidity: ", c1.read().decode(), " Temperature Celsius: ", c2.read().decode(), " CO2: ", co2.read().decode())
         sensor_data['temperature'] = c1.read().decode()
         sensor_data['humidity'] = c2.read().decode()
+        sensor_data['co2'] = co2.read().decode()
         client.publish('v1/devices/me/telemetry', json.dumps(sensor_data), 1)
         #print(c1.read().decode())
         #print(c2.read().decode())
